32236ba349402c73f68739a165500bc9c56a56cb
dchaika1990 <dchaika1990@gmail.com>
2018-03-16 00:46:13 +0200

    test

--- TasksJS/js/init_login.js
+++
@@ -1,7 +0,0 @@
-
-let login_form = new ValidateLogin('login-form', {
-    method: 'POST',
-    url: "https://easycode-test-auth-server.herokuapp.com/login"
-});
-
-login_form.init();
--- TasksJS/js/init_signup.js
+++
@@ -1,6 +0,0 @@
-let signup_form = new ValidateSignup('signup-form', {
-    method: 'POST',
-    url: 'https://easycode-test-auth-server.herokuapp.com/signup'
-});
-
-signup_form.init();
